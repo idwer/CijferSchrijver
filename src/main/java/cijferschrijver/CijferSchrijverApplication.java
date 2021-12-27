5d5a3883f1bcc25606ca1e9dbefaba2cde70815c
idwer <idwer@localhost>
2021-12-27 23:21:33 +0100

    add generated project structure

---
+++ src/main/java/cijferschrijver/CijferSchrijverApplication.java
@@ -0,0 +1,13 @@
+package cijferschrijver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CijferSchrijverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CijferSchrijverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cijferschrijver/CijferSchrijverApplicationTests.java
@@ -0,0 +1,13 @@
+package cijferschrijver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CijferSchrijverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
