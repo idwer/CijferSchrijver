5b2fe02f28852fa8f8d6585e20f70c254de82c6e
idwer <idwer@localhost>
2022-01-08 16:48:01 +0100

    frontend/App.js: rework link ordering

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,10 +4,10 @@ import {Link, BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import GetStudents from "./GetStudents";
 import AddStudent from "./AddStudent";
-import AddModule from "./AddModule";
 import GetModules from "./GetModules";
-import AddSemester from "./AddSemester";
+import AddModule from "./AddModule";
 import GetSemesters from "./GetSemesters";
+import AddSemester from "./AddSemester";
 import GetResults from "./GetResults";
 import AddResult from "./AddResult";
 
@@ -23,20 +23,20 @@ function App() {
                     <Link to="/">Home </Link><br/>
                     <Link to="/get/students"> List of Students</Link><br/>
                     <Link to="/add/student"> Add Student</Link><br/>
-                    <Link to="/add/module"> Add Module</Link><br/>
                     <Link to="/get/modules"> List of Modules</Link><br/>
-                    <Link to="/add/semester"> Add Semester</Link><br/>
+                    <Link to="/add/module"> Add Module</Link><br/>
                     <Link to="/get/semesters"> List of Semesters</Link><br/>
+                    <Link to="/add/semester"> Add Semester</Link><br/>
                     <Link to="/get/results"> List of Results</Link><br/>
                     <Link to="/add/results"> Add Result</Link><br/>
                 </nav>
             <Routes>
                 <Route path="/get/students" element={<GetStudents />} />
                 <Route path="/add/student" element={<AddStudent />} />
-                <Route path="/add/module" element={<AddModule />} />
                 <Route path="/get/modules" element={<GetModules />} />
-                <Route path="/add/semester" element={<AddSemester />} />
+                <Route path="/add/module" element={<AddModule />} />
                 <Route path="/get/semesters" element={<GetSemesters />} />
+                <Route path="/add/semester" element={<AddSemester />} />
                 <Route path="/get/results" element={<GetResults />} />
                 <Route path="/add/results" element={<AddResult />} />
             </Routes>
